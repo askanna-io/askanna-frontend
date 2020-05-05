2bc3f2f281aa6cb14a47c790499a836a3dcda460
Anrii <andrii@askanna.io>
2020-05-05 11:13:12 +0300

    [IMP] - #44 added new functions to view input file in different format types, refactored JobRun component

---
+++ src/features/jobrun/components/jobrun/JobRunLog.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-expansion-panel>
+    <v-expansion-panel-header>Log</v-expansion-panel-header>
+    <v-expansion-panel-content> </v-expansion-panel-content>
+  </v-expansion-panel>
+</template>
+<script lang="ts">
+import { JobRun } from '../../store/types'
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'JobRunLog',
+
+  props: {
+    jobRun: {
+      type: Object as () => JobRun
+    }
+  }
+})
+</script>
---
+++ src/features/jobrun/components/jobrun/JobRunResult.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-expansion-panel>
+    <v-expansion-panel-header>Result</v-expansion-panel-header>
+    <v-expansion-panel-content>
+      <v-list class="transparent" max-width="450px">
+        <v-list-item>
+          <v-list-item-title>Result:</v-list-item-title>
+          <v-list-item-title class="text-left">
+            {{ jobRun.return_payload }}
+          </v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-expansion-panel-content>
+  </v-expansion-panel>
+</template>
+<script lang="ts">
+import { JobRun } from '../../store/types'
+import { defineComponent } from '@vue/composition-api'
+
+export default defineComponent({
+  name: 'JobRunResult',
+
+  props: {
+    jobRun: {
+      type: Object as () => JobRun
+    }
+  }
+})
+</script>
