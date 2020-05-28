0e8234b306edec4dd9c4e918aa636e0be2c8aa04
Anrii <andrii@askanna.io>
2020-05-28 12:32:37 +0300

    delete : from titles, change cutup

--- src/features/job/components/overview/JobDefinition.vue
+++ src/features/job/components/overview/JobDefinition.vue
@@ -1,15 +1,17 @@
 <template>
-  <v-container class="ma-0" fluid>
-    <v-card-title>Definition:</v-card-title>
-    <v-row>
-      <v-col cols="2" sm="6"> <span>File: </span>File.name </v-col>
-      <v-col cols="2" sm="6"> <span>Code: </span>Package.v1 </v-col>
-    </v-row>
-    <v-row>
-      <v-col cols="6" sm="6"> <span>Function: </span>{{ job.function }}</v-col>
-      <v-col cols="6" sm="6"> <span>Environment: </span>{{ job.environment }}</v-col>
-    </v-row>
-  </v-container>
+  <div>
+    <v-card-title>Definition</v-card-title>
+    <v-container class="ma-1 fluid">
+      <v-row>
+        <v-col cols="2" sm="6"> <span>File: </span>File.name </v-col>
+        <v-col cols="2" sm="6"> <span>Code: </span>Package.v1 </v-col>
+      </v-row>
+      <v-row>
+        <v-col cols="6" sm="6"> <span>Function: </span>{{ job.function }}</v-col>
+        <v-col cols="6" sm="6"> <span>Environment: </span>{{ job.environment }}</v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script>
