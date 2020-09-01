f3c6c2c4362ede3d5b298420077dd648343840cf
Robbert <robbert@askanna.io>
2020-09-01 20:35:57 +0200

    Move location of download askanna.yml button

--- src/features/package/components/resumable-upload/NewPackageInfo.vue
+++ src/features/package/components/resumable-upload/NewPackageInfo.vue
@@ -24,10 +24,7 @@
 
         <p>
           In the <span class="font-italic">askanna.yml</span>, you add the next line. Adding this line is the only
-          configuration you need to do.
-        </p>
-        <p>
-          <ask-anna-copy-text :text="projectUrl" />
+          configuration you need to do. Or you can download
           <v-tooltip top>
             <template v-slot:activator="{ on }">
               <v-btn v-on="on" class="ml-1" x-small outlined color="secondary" @click.stop="handleDownload">
@@ -35,7 +32,10 @@
               </v-btn>
             </template>
             <span>Download <i>askanna.yml</i></span>
-          </v-tooltip>
+          </v-tooltip> and use this <span class="font-italic">askanna.yml</span> file.
+        </p>
+        <p>
+          <ask-anna-copy-text :text="projectUrl" />
         </p>
         <p>
           In your terminal, make sure you are in one of the project directories. Now you can run
