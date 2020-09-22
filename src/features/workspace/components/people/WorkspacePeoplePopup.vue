2bb956431805b1d60e85fce3cea113256bb629ec
Anrii <andrii@askanna.io>
2020-09-22 12:19:55 +0300

    [IMP] -#120 change cutup for people popup

--- src/features/workspace/components/people/WorkspacePeoplePopup.vue
+++ src/features/workspace/components/people/WorkspacePeoplePopup.vue
@@ -1,16 +1,22 @@
 <template>
   <v-row justify="center">
-    <v-dialog v-model="openVmodel" max-width="350px">
-      <v-card class="text-center positin">
-        <div class="avatar--wrapper">
-          <v-avatar rounded="21" class="ma-2" size="150" tile>
-            <v-img v-if="people.avatar" class="img--rounded" :src="people.avatar" />
-            <v-icon v-else size="55">mdi-account-outline</v-icon>
-          </v-avatar>
-          <v-chip class="role" color="primary">
-            {{ people.role }}
-          </v-chip>
-        </div>
+    <v-dialog v-model="openVmodel" max-width="400px">
+      <v-card class="text-center position">
+        <v-container pa-0 class="avatar--wrapper">
+          <v-row>
+            <v-col class="pt-2 pb-0" cols="12" align-self="start">
+              <v-avatar rounded="21" :size="people.avatar ? 150 : 50" tile>
+                <v-img v-if="people.avatar" class="img--rounded" :src="people.avatar" />
+                <v-icon v-else size="55">mdi-account-outline</v-icon>
+              </v-avatar>
+            </v-col>
+            <v-col :class="[people.avatar ? 'pt-1' : 'pt-0']">
+              <v-chip class="role" color="primary">
+                {{ people.role }}
+              </v-chip>
+            </v-col>
+          </v-row>
+        </v-container>
         <v-card-text>
           <v-container pa-0>
             <v-row>
@@ -36,7 +42,7 @@
               </div>
               <div class="mt-2">
                 <v-btn small block outlined text color="secondary" class="btn--hover" @click="handleRemove">
-                  Remove&nbsp;<span class="error--text">{{ people.name }}</span
+                  Remove&nbsp;<br /><span class="error--text">{{ people.name }}</span
                   >&nbsp;from {{ workspaceName }}
                 </v-btn>
               </div>
@@ -100,13 +106,9 @@ export default defineComponent({
 </script>
 <style scoped>
 .avatar--wrapper {
-  position: relative;
 }
 .role {
   white-space: nowrap;
   display: inline-block;
-  position: absolute;
-  bottom: -3px;
-  left: 41%;
 }
 </style>
