3c100d30623c3aadea7c8966f98456c1bbf8e2bb
Anrii <andrii@askanna.io>
2020-11-04 14:32:33 +0200

    [IMP] -#144 testing

--- src/core/layouts/dashboard.vue
+++ src/core/layouts/dashboard.vue
@@ -220,7 +220,7 @@ export default defineComponent({
   components: { UpdateApp, TheUploadStatus },
 
   setup(props, context) {
-    console.log('from dashborasd')
+    console.log('from dashborasd 2 2 2 2 2 2 2 2 2 2 2 2 2')
     useTitle(context)
     const authStore = useAuthStore()
     const projectStore = useProjectStore()
