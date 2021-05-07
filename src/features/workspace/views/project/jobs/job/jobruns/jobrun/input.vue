7f03f61ce7326e576841da41b8a1e13b48b81991
Anrii <andrii@askanna.io>
2021-05-07 12:20:20 +0300

    [IMP] -#204 add stop loading on empty payload

--- src/features/workspace/views/project/jobs/job/jobruns/jobrun/input.vue
+++ src/features/workspace/views/project/jobs/job/jobruns/jobrun/input.vue
@@ -5,7 +5,7 @@
 <script>
 import useJobRunStore from '@jobrun/composition/useJobRunStore'
 import JobRunInput from '@/features/jobrun/components/jobrun/JobRunInput.vue'
-import { watch, computed, onBeforeMount, defineComponent } from '@vue/composition-api'
+import { watch, computed, onBeforeMount, onMounted, defineComponent } from '@vue/composition-api'
 
 export default defineComponent({
   components: { JobRunInput },
@@ -17,14 +17,22 @@ export default defineComponent({
     const jobRunShortId = computed(() => jobRunStore.jobRun.value.short_uuid)
     const payloadUuid = computed(() => jobRunStore.jobRun.value.payload.short_uuid)
 
+    const stopLoading = async () => await jobRunStore.setLoading({ name: 'payLoadLoading', value: false })
+
     const handleViewPayload = async (jobRunShortId, payloadUuid) => {
       await jobRunStore.setLoading({ name: 'payLoadLoading', value: true })
 
       if (payloadUuid) {
         await jobRunStore.getJobRunPayload({ jobRunShortId, payloadUuid })
-        await jobRunStore.setLoading({ name: 'payLoadLoading', value: false })
+        await stopLoading()
       }
     }
+
+    watch(jobRunShortId, jobRunShortId => {
+      if (!payloadUuid.value && jobRunShortId) {
+        stopLoading()
+      }
+    })
 
     watch(payloadUuid, (payloadUuid, previusPayloadUuid) => {
       if (!jobRunStore.jobRunPayload.value || previusPayloadUuid !== payloadUuid) {
@@ -35,6 +43,12 @@ export default defineComponent({
     onBeforeMount(() => {
       if (jobRunShortId.value !== jobRunId || !jobRunStore.jobRunPayload.value) {
         handleViewPayload(jobRunShortId.value, payloadUuid.value)
+      }
+    })
+
+    onMounted(() => {
+      if (!payloadUuid.value && jobRunShortId.value) {
+        stopLoading()
       }
     })
   }
