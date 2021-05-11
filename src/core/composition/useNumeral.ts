d5d096c72a05a9cc6a02fb3746ff29858fcbb542
Anrii <andrii@askanna.io>
2021-05-11 20:12:28 +0300

    [IMP] -#209 added function to cal decimals, refactored function to show number is different formats

--- src/core/composition/useNumeral.ts
+++ src/core/composition/useNumeral.ts
@@ -3,9 +3,16 @@ import { SetupContext } from '@vue/composition-api'
 
 export default function (context: SetupContext) {
   const numberFormated = (num: string | number) => {
-    const numberFormat = Number(num) % 1 ? '0,0.000' : '0,0'
+    const decs = countDecimals(Number(num))
+    const formats = ['0,0', '0,0.0', '0,0.00', '0,0.000']
+    const numberFormat = formats[decs] || '0,0.000'
 
     return numeral(num).format(numberFormat)
+  }
+
+  const countDecimals = (val: number) => {
+    if (Math.floor(val) === val) return 0
+    return val.toString().split('.')[1].length || 0
   }
 
   return { numberFormated }
