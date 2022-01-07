ceeb3c87dc5bbc59ed00b51243c85c551fad5dcd
Anrii <andrii@askanna.io>
2022-01-07 12:50:08 +0200

    [IMP] -#CU-9yzm1m added support for copy image

--- src/features/package/components/PackageFileImage.vue
+++ src/features/package/components/PackageFileImage.vue
@@ -13,35 +13,25 @@
   </v-container>
 </template>
 
-<script>
-import { computed, defineComponent } from '@vue/composition-api'
+<script setup lang="ts">
+import { computed } from '@vue/composition-api'
 
-export default defineComponent({
-  name: 'PackageFileImage',
-
-  props: {
-    dataSource: {
-      type: String | Blob,
-      default: function () {
-        return ''
-      }
-    }
-  },
-
-  setup(props) {
-    const imgSrc = computed(() => {
-      if (!props.dataSource) return ''
-
-      const urlCreator = window.URL || window.webkitURL
-      const imgSrc = urlCreator.createObjectURL(props.dataSource)
-
-      return imgSrc
-    })
-
-    return {
-      imgSrc
+const props = defineProps({
+  dataSource: {
+    type: Blob,
+    default: function () {
+      return ''
     }
   }
+})
+
+const imgSrc = computed(() => {
+  if (!props.dataSource) return ''
+
+  const urlCreator = window.URL || window.webkitURL
+  const imgSrc = urlCreator.createObjectURL(props.dataSource)
+
+  return imgSrc
 })
 </script>
 <style scoped>
