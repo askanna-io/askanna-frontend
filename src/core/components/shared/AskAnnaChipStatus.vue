4eeaf5e8b11b6d267e9b784610b638603a14cada
Andrii Shapovalov <andrii@askanna.io>
2022-01-12 22:17:05 +0100

    Use same status colors as we use in notification email - CU-pxc02m

--- src/core/components/shared/AskAnnaChipStatus.vue
+++ src/core/components/shared/AskAnnaChipStatus.vue
@@ -7,65 +7,58 @@
   </v-chip>
 </template>
 
-<script>
-import { computed, defineComponent } from '@vue/composition-api'
+<script setup lang="ts">
+import { get } from 'lodash'
+import { computed } from '@vue/composition-api'
 
-export default defineComponent({
-  name: 'AskAnnaChipStatus',
-
-  props: {
-    status: String
-  },
-
-  setup(props) {
-    const COLORS = {
-      SUCCESS: 'success',
-      COMPLETED: 'success',
-      UNDEFINED: 'grey',
-      FAILURE: 'error',
-      FAILED: 'error',
-      IN_PROGRESS: 'yellow darken-2',
-      RUNNING: 'yellow darken-2',
-      SUBMITTED: 'amber lighten-1',
-      PENDING: 'blue lighten-3',
-      QUEUED: 'blue lighten-3',
-      FINISHED: 'success'
-    }
-    const ICONS = {
-      UNDEFINED: 'mdi-do-not-disturb',
-      FAILURE: 'mdi-alert-circle-outline',
-      FAILED: 'mdi-alert-circle-outline',
-      SUCCESS: 'mdi-checkbox-marked-circle',
-      COMPLETED: 'mdi-checkbox-marked-circle',
-      PENDING: 'mdi-progress-clock',
-      QUEUED: 'mdi-progress-clock',
-      SUBMITTED: 'mdi-progress-check',
-      IN_PROGRESS: 'mdi-progress-clock',
-      RUNNING: 'mdi-progress-clock',
-      FINISHED: 'mdi-checkbox-marked-circle'
-    }
-    const TEXTS = {
-      SUCCESS: 'Succeeded',
-      COMPLETED: 'Succeeded',
-      UNDEFINED: 'Undefined',
-      FAILURE: 'Failure',
-      FAILED: 'Failure',
-      PENDING: 'Pending',
-      QUEUED: 'Pending',
-      SUBMITTED: 'Submitted',
-      IN_PROGRESS: 'In progress',
-      RUNNING: 'In progress',
-      FINISHED: 'Finished'
-    }
-    const status = computed(() => (props.status ? props.status.toUpperCase() : 'UNDEFINED'))
-
-    const statusIcon = computed(() => ICONS[status.value])
-    const statusColor = computed(() => COLORS[status.value])
-    const statusValue = computed(() => TEXTS[status.value])
-
-    return { statusColor, statusIcon, statusValue }
-  }
+const props = defineProps({
+  status: String
 })
+
+const COLORS = {
+  SUCCESS: 'success',
+  COMPLETED: 'success',
+  UNDEFINED: 'grey',
+  FAILURE: 'error',
+  FAILED: 'error',
+  IN_PROGRESS: 'blue lighten-3',
+  RUNNING: 'blue lighten-3',
+  SUBMITTED: 'grey',
+  PENDING: 'grey',
+  QUEUED: 'grey',
+  FINISHED: 'success'
+}
+const ICONS = {
+  UNDEFINED: 'mdi-do-not-disturb',
+  FAILURE: 'mdi-alert-circle-outline',
+  FAILED: 'mdi-alert-circle-outline',
+  SUCCESS: 'mdi-checkbox-marked-circle',
+  COMPLETED: 'mdi-checkbox-marked-circle',
+  PENDING: 'mdi-progress-clock',
+  QUEUED: 'mdi-progress-clock',
+  SUBMITTED: 'mdi-progress-check',
+  IN_PROGRESS: 'mdi-progress-clock',
+  RUNNING: 'mdi-progress-clock',
+  FINISHED: 'mdi-checkbox-marked-circle'
+}
+const TEXTS = {
+  SUCCESS: 'Succeeded',
+  COMPLETED: 'Succeeded',
+  UNDEFINED: 'Undefined',
+  FAILURE: 'Failure',
+  FAILED: 'Failure',
+  PENDING: 'Pending',
+  QUEUED: 'Pending',
+  SUBMITTED: 'Submitted',
+  IN_PROGRESS: 'In progress',
+  RUNNING: 'In progress',
+  FINISHED: 'Finished'
+}
+const status = computed(() => (props.status ? props.status.toUpperCase() : 'UNDEFINED'))
+
+const statusIcon = computed(() => get(ICONS, status.value))
+const statusColor = computed(() => get(COLORS, status.value))
+const statusValue = computed(() => get(TEXTS, status.value))
 </script>
 <style>
 .askaanna-chip-status {
