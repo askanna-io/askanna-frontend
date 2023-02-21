747ea0c71f12e414d0843559e4b33ab917b94d87
Andrii Shapovalov <andrii@askanna.io>
2023-02-21 17:45:38 +0100

    Add extension to not preview

--- src/composables/useFileExtension.ts
+++ src/composables/useFileExtension.ts
@@ -3,12 +3,12 @@ export default () => {
     pdf: ['pdf'],
     xls: ['xls', 'xlsx'],
     csv: ['csv', 'tsv'],
-    noPreview: ['h5', 'pkl', 'parquet'],
     images: ['jpg', 'png', 'gif', 'jpeg'],
     notShowCopyBtn: ['pdf', 'pkl', 'parquet', 'xls', 'xlsx'],
     isRendered: ['html', 'json', 'md', 'markdown', 'csv', 'tsv'],
     bigPreview: ['html', 'ipynb', 'csv', 'tsv', 'xls', 'xlsx', 'pdf'],
-    code: ['xml', 'csv', 'tsv', 'plain', 'json', 'md', 'py', 'txt', 'yml', 'ini', 'toml', 'markdown']
+    code: ['xml', 'csv', 'tsv', 'plain', 'json', 'md', 'py', 'txt', 'yml', 'ini', 'toml', 'markdown'],
+    noPreview: ['h5', 'pkl', 'parquet', 'app', 'exe', 'com', 'dll', 'dmg', 'iso', 'jar', 'zip', 'ko', 'lib', 'so', 'dat', 'bin', 'cab']
   }
   return extenstions
 }
